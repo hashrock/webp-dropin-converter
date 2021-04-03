f2882809541c4a7680f5bd0bcaaf7d7767645fae
hashrock <hashrock@users.noreply.github.com>
2021-04-03 22:41:06 +0900

    cleanup

--- webp.js
+++ webp.js
@@ -1,59 +1,51 @@
 let api
 Module.onRuntimeInitialized = async (_) => {
-    api = {
-      version: Module.cwrap("version", "number", []),
-      create_buffer: Module.cwrap("create_buffer", "number", [
-        "number",
-        "number",
-      ]),
-      destroy_buffer: Module.cwrap("destroy_buffer", "", ["number"]),
-      encode: Module.cwrap("encode", "", [
-        "number",
-        "number",
-        "number",
-        "number",
-      ]),
-      free_result: Module.cwrap("free_result", "", ["number"]),
-      get_result_pointer: Module.cwrap("get_result_pointer", "number", []),
-      get_result_size: Module.cwrap("get_result_size", "number", []),
-    };
+  api = {
+    version: Module.cwrap("version", "number", []),
+    create_buffer: Module.cwrap("create_buffer", "number", [
+      "number",
+      "number",
+    ]),
+    destroy_buffer: Module.cwrap("destroy_buffer", "", ["number"]),
+    encode: Module.cwrap("encode", "", [
+      "number",
+      "number",
+      "number",
+      "number",
+    ]),
+    free_result: Module.cwrap("free_result", "", ["number"]),
+    get_result_pointer: Module.cwrap("get_result_pointer", "number", []),
+    get_result_size: Module.cwrap("get_result_size", "number", []),
   };
+};
 
-  async function convertWebp(imageBlob){
-    const image = await loadImage(imageBlob);
-    const p = api.create_buffer(image.width, image.height);
-    Module.HEAP8.set(image.data, p);
-    // ... call encoder ...
+async function convertWebp(imageBlob) {
+  const image = await loadImage(imageBlob);
+  const p = api.create_buffer(image.width, image.height);
+  Module.HEAP8.set(image.data, p);
 
-    api.encode(p, image.width, image.height, 75);
-    const resultPointer = api.get_result_pointer();
-    const resultSize = api.get_result_size();
-    const resultView = new Uint8Array(
-      Module.HEAP8.buffer,
-      resultPointer,
-      resultSize
-    );
-    const result = new Uint8Array(resultView);
-    api.free_result(resultPointer);
-    const blob = new Blob([result], { type: "image/webp" });
-    // const blobURL = URL.createObjectURL(blob);
-    // const img = document.createElement("img");
-    // img.src = blobURL;
-    // document.body.appendChild(img);
+  api.encode(p, image.width, image.height, 75);
+  const resultPointer = api.get_result_pointer();
+  const resultSize = api.get_result_size();
+  const resultView = new Uint8Array(
+    Module.HEAP8.buffer,
+    resultPointer,
+    resultSize
+  );
+  const result = new Uint8Array(resultView);
+  api.free_result(resultPointer);
+  const blob = new Blob([result], { type: "image/webp" });
 
-    api.destroy_buffer(p);
-    return blob
+  api.destroy_buffer(p);
+  return blob
 }
 
 
-  async function loadImage(img) {
-    // const img = await createImageBitmap(imgBlob);
-    // Make canvas same size as image
-    const canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    // Draw image onto canvas
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-    return ctx.getImageData(0, 0, img.width, img.height);
-  }
+async function loadImage(img) {
+  const canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+  return ctx.getImageData(0, 0, img.width, img.height);
+}
